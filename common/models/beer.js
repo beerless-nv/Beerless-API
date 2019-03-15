20445dea4016a1e528eb963f20dad3623a562d88
Tomnuyts1 <r0661550@student.thomasmore.be>
2019-03-15 14:04:45 +0100

    added entry endpoints

--- common/models/beer.js
+++ common/models/beer.js
@@ -1,5 +1,39 @@
 'use strict';
 
 module.exports = function(Beer) {
+  Beer.beerEntry = async function(data) {
 
+    // check if isApproved exists
+    if (!data['isApproved']) {
+
+      // insert beer object in db
+      data['isApproved'] = 0;
+      // const beer = Beer.create(data);
+
+      // get userId
+      // const token = options && options.accessToken;
+
+
+      // // add activity for the insert
+      // const activity = {
+      //   "timestampCreated": "1970-01-01T00:00:00.000Z",
+      //   "timestampUpdated": "1970-01-01T00:00:00.000Z",
+      //   "beerId": beer['id'],
+      //   "breweryId": 0,
+      //   "articleId": 0,
+      //   "userId": 0,
+      //   "activityTypeId": 0,
+      //   "isApproved": 0
+      // };
+      //
+      // Beer.app.models.Activity.create(activity);
+      return currentUser;
+    }
+  };
+
+  Beer.remoteMethod('beerEntry', {
+    accepts: {arg: 'data', type: 'object', http: {source: 'body'}, required: true},
+    returns: {type: 'object', root: true},
+    http: {path: '/beerEntry', verb: 'post'},
+  });
 };
--- common/models/brewery.js
+++ common/models/brewery.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function(Brewery) {
+  Brewery.breweryEntry = async function(data) {
+    if (!data['isApproved']) {
+      data['isApproved'] = 0;
+      Brewery.create(data);
+      Activity.create()
+      return data;
+    }
+  };
 
+  Brewery.remoteMethod('breweryEntry', {
+    accepts: {arg: 'data', type: 'object', http: {source: 'body'}, required: true},
+    returns: {type: 'object', root: true},
+    http: {path: '/breweryEntry', verb: 'post'},
+  });
 };
