4fa122b0e34a7e13356a81aeb5954796675fcc27
Tomnuyts1 <r0661550@student.thomasmore.be>
2019-03-19 17:21:44 +0100

    Bug fix in averages functions from tastingprofiles

--- common/models/tastingprofile.js
+++ common/models/tastingprofile.js
@@ -2,43 +2,6 @@
 
 module.exports = function(Tastingprofile) {
 
-  /**
-   * Get the average tastingprofiles of a single beer.
-   *
-   * Averages the tasting characteristic columns.
-   *
-   * @param data
-   * @returns {Promise<void>}
-   */
-
-  Tastingprofile.averages = async function(data) {
-    // create sql query
-    const query = 'SELECT * FROM Tastingprofile';
-    // const sql_stmt = "SELECT ID, AVG(malty), AVG(sweet), AVG(sour), AVG(hoppy), AVG(bitter), AVG(fruity), userID, beerID FROM Tastingprofile GROUP BY beerID";
-
-    // define datasource
-    const ds = Tastingprofile.dataSource;
-
-    // execute query on database
-    let resultQuery = null;
-    resultQuery = new Promise(resolve => {
-      ds.connector.query(query, function(err, result) {
-        resolve(result);
-      });
-    });
-
-    return resultQuery;
-  };
-
-  Tastingprofile.remoteMethod('averages', {
-    accepts: {
-      arg: 'data',
-      type: 'number',
-      required: true,
-    },
-    returns: {type: 'object', root: true},
-    http: {path: '/averages/{id}', verb: 'get'},
-  });
 
   /**
    * Get the average tastingprofiles of all beers.
@@ -50,16 +13,15 @@ module.exports = function(Tastingprofile) {
    * @returns {Promise<void>}
    */
 
-  Tastingprofile.averages = async function() {
+  Tastingprofile.getAllAverages = async function() {
     // create sql query
-    const query = "SELECT ID, AVG(malty) AS malty, AVG(sweet) AS sweet, AVG(sour) AS sour, AVG(hoppy) AS hoppy, AVG(bitter) AS bitter, AVG(fruity) AS fruity, userID, beerID FROM Tastingprofile GROUP BY beerID";
+    const query = "SELECT ID, AVG(malty) AS malty, AVG(sweet) AS sweet, AVG(sour) AS sour, AVG(hoppy) AS hoppy, AVG(bitter) AS bitter, AVG(fruity) AS fruity, beerID FROM Tastingprofile GROUP BY beerID";
 
     // define datasource
     const ds = Tastingprofile.dataSource;
 
     // execute query on database
-    let resultQuery = null;
-    resultQuery = new Promise(resolve => {
+    let resultQuery = new Promise(resolve => {
       ds.connector.query(query, function(err, result) {
         resolve(result);
       });
@@ -68,8 +30,48 @@ module.exports = function(Tastingprofile) {
     return resultQuery;
   };
 
-  Tastingprofile.remoteMethod('averages', {
+  Tastingprofile.remoteMethod('getAllAverages', {
     returns: {type: 'object', root: true},
     http: {path: '/averages', verb: 'get'},
+  });
+
+
+  /**
+   * Get the average tastingprofiles of a single beer.
+   *
+   * Averages the tasting characteristic columns.
+   *
+   * @param id
+   * @returns {Promise<void>}
+   */
+
+  Tastingprofile.getAverages = async function(id) {
+    // catch beerId from request param
+    const beerId = id;
+
+    // create sql query
+    const query = "SELECT ID, AVG(malty) AS malty, AVG(sweet) AS sweet, AVG(sour) AS sour, AVG(hoppy) AS hoppy, AVG(bitter) AS bitter, AVG(fruity) AS fruity, beerID FROM Tastingprofile WHERE beerID = ?";
+
+    // define datasource
+    const ds = Tastingprofile.dataSource;
+
+    // execute query on database
+    let resultQuery = new Promise(resolve => {
+      ds.connector.query(query, [beerId], function(err, result) {
+        resolve(result);
+      });
+    });
+
+    return resultQuery;
+  };
+
+  Tastingprofile.remoteMethod('getAverages', {
+    accepts: {
+      arg: 'id',
+      type: 'number',
+      required: true,
+    },
+    http: {path: '/averages/:id', verb: 'get'},
+    returns: {type: 'object', root: true},
   });
 };
