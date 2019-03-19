c0d0efe70a73c278d519d72880ba581422b076c3
Tomnuyts1 <r0661550@student.thomasmore.be>
2019-03-19 14:27:54 +0100

    Added sliding expiration of access tokens

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var app = module.exports = loopback();
 
 // user context
 app.use(loopback.token());
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   if (!req.accessToken) return next();
   app.models.UserFull.findById(req.accessToken.userId, function(err, user) {
     if (err) return next(err);
@@ -20,6 +20,13 @@ app.use(function (req, res, next) {
     next();
   });
 });
+
+// access token
+app.use(loopback.token({
+  cookies: ['access_token'],
+  headers: ['access_token', 'X-Access-Token'],
+  params: ['access_token'],
+}));
 
 app.start = function() {
   // start the web server
@@ -40,6 +47,23 @@ boot(app, __dirname, function(err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module)
+  if (require.main === module) {  // if we are running loopback as our server
+    app.use(loopback.token({}));
+
+    //
+    // Sliding expiration of access tokens
+    //
+    app.use(function updateToken(req, res, next) {
+      var token = req.accessToken; // get the accessToken from the request
+      if (!token) return next(); // if there's no token we use next() to delegate handling back to loopback
+      var now = new Date();
+      // EDIT: to make sure we don't use a token that's already expired, we don't update it
+      // this line is not really needed, because the framework will catch invalid tokens already
+      if (token.created.getTime() + (token.ttl * 1000) < now.getTime()) return next();
+      // performance optimization, we do not update the token more often than once per day
+      if (now.getTime() - token.created.getTime() < 86400000) return next();
+      token.updateAttribute('created', now, next); // save to db and move on
+    });
     app.start();
+  }
 });
