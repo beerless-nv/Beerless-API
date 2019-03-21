575b8771175d35beac1aebfc190d90141225eb05
Tomnuyts1 <r0661550@student.thomasmore.be>
2019-03-21 14:43:21 +0100

    removed previous mixin

--- common/mixins/timeStamp.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(Model, options) {
-  // Model is the model class
-  // options is an object containing the config properties from model definition
-  Model.defineProperty('timestampCreated', {type: Date, default: Date.now()});
-  Model.defineProperty('timestampUpdated', {type: Date, default: Date.now()});
-};
